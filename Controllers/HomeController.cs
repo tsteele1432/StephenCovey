df36360c2f0b5f39e981170c337daae4d8433c10
rickersb <80800979+rickersb@users.noreply.github.com>
2022-02-03 15:21:53 -0700

    firt-commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Covey.Models;
+
+namespace Covey.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+
+        public IActionResult Quadrants()
+        {
+            return View();
+        }
+    }
+}
